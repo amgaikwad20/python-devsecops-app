75552089a3adca17b4da94c0c0ba6dae18d5269e
Amol gaikwad <amgaikwad2025@gmail.com>
2026-08-27 06:43:44 -0700

    first commit

---
+++ test_app.py
@@ -0,0 +1,14 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello from Python DevSecOps Pipeline!" in response.data
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
